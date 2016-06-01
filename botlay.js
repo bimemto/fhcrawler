9cbcac48ecfb373b0c5b15851301f1318c64aed6
Nguyễn Khánh Duy <duynk@appota.com>
2016-06-01 11:41:57 +0700

    add bot tho

--- botlay.js
+++ botlay.js
@@ -8,6 +8,7 @@ var banquo = require('banquo');
 var fs = require("fs");
 var db = require("./db.js")
 var dateFormat = require("dateformat");
+var request = require('request');
 
 var token = '208861476:AAFkV6kx6rjKOOyNQudcZ88YrTH6ZATCRIo';
 // Setup polling way
@@ -257,6 +258,42 @@ else if (event.body.indexOf('/tt') > -1) {
         api.sendMessage(msg, event.threadID);
       }, 1200);
 
+    }
+  });
+} else if(event.body.indexOf('/tho') > -1){
+  api.markAsRead(event.threadID, function(err) {
+    if (err) console.log(err);
+  });
+  var words = '';
+  if (event.body.length > 5) {
+    words = event.body.split(' ')[1];
+  }
+  request.post('http://thomay.vn/index.php?q=tutaochude2', 
+    {form: {
+      'dieukien_tu': '',
+      'dieukien_tu_last': '',
+      'fullbaitho': 'Thêm một khổ',
+      'last': '',
+      'order': '0',
+      'order_cu': '0',
+      'poem': '',
+      'poemSubject_tutao': '1',
+      'poemType': 'Lục bát',
+      'theloai': 'tho',
+      'tulap[cu]': '',
+      'tulap[moi]': '',
+      'tungcau_kho': '',
+      'tunhap_chude': words,
+      'van[cu]': '',
+      'van[moi]': '',
+    }
+  }, function(error, response, body){
+    if(error) {
+      console.log(error);
+    } else {
+        $ = cheerio.load(body, { decodeEntities: false });
+        var tho = $('font').attr('color', 'Blue').html().split('<br>').join('\r\n');
+        api.sendMessage(tho, event.threadID);
     }
   });
 }
--- server.js
+++ server.js
@@ -1,84 +1,31 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
-var path = require('path');
+var request = require('request');
+var cheerio = require('cheerio');
 
-var async = require('async');
-var socketio = require('socket.io');
-var express = require('express');
-
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
-
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
-
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
-
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
+request.post('http://thomay.vn/index.php?q=tutaochude2', 
+    {form: {
+      'dieukien_tu': '',
+      'dieukien_tu_last': '',
+      'fullbaitho': 'Thêm một khổ',
+      'last': '',
+      'order': '0',
+      'order_cu': '0',
+      'poem': '',
+      'poemSubject_tutao': '1',
+      'poemType': 'Lục bát',
+      'theloai': 'tho',
+      'tulap[cu]': '',
+      'tulap[moi]': '',
+      'tungcau_kho': '',
+      'tunhap_chude': 'duy',
+      'van[cu]': '',
+      'van[moi]': '',
     }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
+  }, function(error, response, body){
+    if(error) {
+      console.log(error);
+    } else {
+        $ = cheerio.load(body, { decodeEntities: false });
+        var tho = $('font').attr('color', 'Blue').html().split('<br>').join('\r\n');
+        console.log(tho);
+    }
   });
-}
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
-});
