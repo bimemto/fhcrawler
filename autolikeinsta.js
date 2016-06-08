5aba76330b2caf1efee204c8040e79d6f5ddaf17
Nguyễn Khánh Duy <duynk@appota.com>
2016-06-08 17:17:32 +0700

    add insta scopr

--- autolikeinsta.js
+++ autolikeinsta.js
@@ -1,10 +1,16 @@
 var http = require('http');
 var express = require('express');
-var api = require('instagram-node').instagram();
+var ig = require('instagram-node').instagram();
 var app = express();
+var request = require('request');
 
 // app.configure(function() {
 //   // The usual...
+// });
+// var accessToken = '32371706.36dd655.26b39f4bdf2a427d83a6d5674dc3a213';
+
+// request.get('https://api.instagram.com/v1/users/self/feed?access_token=' + accessToken, function(req, res){
+// 	console.log(res);
 // });
 
 api.use({
@@ -15,7 +21,7 @@ api.use({
 var redirect_uri = 'http://bu.1ly.co:8080/handleauth';
 
 exports.authorize_user = function(req, res) {
-  res.redirect(api.get_authorization_url(redirect_uri, { scope: ['likes'], state: 'a state' }));
+  res.redirect(api.get_authorization_url(redirect_uri, { scope: ['likes+basic+comments',], state: 'a state' }));
 };
 
 exports.handleauth = function(req, res) {
@@ -43,7 +49,3 @@ var server = app.listen(8080, function() {
     console.log("Example app listening at http://%s:%s", host, port)
 
 })
-
-// http.createServer(app).listen(app.get('port'), function(){
-//   console.log("Express server listening on port " + app.get('port'));
-// });
