6425081734f9cd460e9b9e53c8f88b69cd00df13
Nguyễn Khánh Duy <duynk@appota.com>
2016-06-22 10:35:07 +0700

    close db

--- live_football_api.js
+++ live_football_api.js
@@ -8,7 +8,6 @@ app.get('/euro/api/match_list', function(req, res) {
     db.getMatchList(function(err, rows) {
         if (err) {
             res.send("Error");
-            db.closeDB();
         }
         else {
             var data = [];
@@ -23,7 +22,6 @@ app.get('/euro/api/match_list', function(req, res) {
                 data[i] = {team_home: team_home, team_away: team_away, logo_home: logo_home, logo_away: logo_away, time: time, league: league, details_url: details_url};
             }
             res.send(data);
-            db.closeDB();
         }
     });
 })
@@ -50,7 +48,6 @@ app.get('/euro/api/get_live_url', function(req, res){
                                 if(iframe2Url.indexOf('youtube.com') > -1  || iframe2Url.indexOf('http://tv.keonhacai.com/talk.php') > -1){
                                     var data = {live_url: iframe2Url};
                                     res.send(data); 
-                                    db.closeDB(); 
                                 } else {
                                     if(iframe2Url.indexOf('http://tv.keonhacai.com/hot') > -1){
                                         iframe2Url = 'http://tv.keonhacai.com/hot/k1_' + server + ".php";
@@ -60,12 +57,10 @@ app.get('/euro/api/get_live_url', function(req, res){
                             } else {
                                 var data = {live_url: ''};
                                 res.send(data);
-                                db.closeDB();
                             }
                         } else {
                             var data = {live_url: ''};
                             res.send(data);
-                            db.closeDB();
                         }
                     }
                     });
@@ -73,12 +68,10 @@ app.get('/euro/api/get_live_url', function(req, res){
                 } else {
                     var data = {live_url: ''};
                     res.send(data);
-                    db.closeDB();
                 }
             } else {
                 var data = {live_url: ''};
                 res.send(data);
-                db.closeDB();
             }
         }
     }).queue(details_url);
