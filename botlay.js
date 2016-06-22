593be411d054e89b7170636d14f8e466e559781d
Nguyễn Khánh Duy <duynk@appota.com>
2016-06-22 11:10:53 +0700

    add girl

--- botlay.js
+++ botlay.js
@@ -21,6 +21,7 @@ var bot = new TelegramBot(token, {
 
 var webshot = require('webshot');
 var tinh_nguoi = true;
+var imgs = [];
 
 function download(url, callback) {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
@@ -512,34 +513,7 @@ bot.on('message', function(message) {
   var converted = degree * 9/5 + 32;
   bot.sendMessage(message.chat.id, degree + ' độ xê = ' + converted + ' độ ép');
 } else if(message.text.indexOf('/img') > -1){
-  new Crawler({
-    maxConnections: 10,
-    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
-    callback: function(error, result, $) {
-      if($){
-        $('div.posts.sub-gallery.br5.first-child').find('div.post').each(function(index, div){
-          var id = $(div).attr('id');
-          var details_url = 'http://imgur.com/r/nsfw/' + id;
-          console.log('details', details_url);
-          new Crawler({
-            maxConnections: 10,
-            userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
-            callback: function(error, result, $) {
-              if($){
-                var img = $('div.post-image').find('img').attr('src');
-                if(img === undefined || img === 'undefined'){
-                  
-                } else {
-                  img = img.substring(2, img.length);
-                  bot.sendMessage(message.chat.id, img);
-                }
-              }
-            }
-          }).queue(details_url);
-        })
-      }
-    }
-  }).queue('http://imgur.com/r/nsfw');
+  bot.sendMessage(message.chat.id, imgs[getRandomInt(0, imgs.length)]);
 }
 }
 });
@@ -581,3 +555,34 @@ function degToCompass(num) {
   var arr = ["Bắc", "Bắc Bắc Đông", "Đông Bắc", "Đông Đông Bắc", "Đông", "Đông Đông Nam", "Đông Nam", "Nam Nam Đông", "Nam", "Nam Nam Tây", "Tây Nam", "Tây Tây Nam", "Tây", "Tây Tây Bắc", "Tây Bắc", "Bắc Bắc Tây"];
   return arr[(val % 16)];
 }
+
+var c = new Crawler({
+    maxConnections: 10,
+    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+    callback: function(error, result, $) {
+      if($){
+        $('div.posts.sub-gallery.br5.first-child').find('div.post').each(function(index, div){
+          var id = $(div).attr('id');
+          var details_url = 'http://imgur.com/r/nsfw/' + id;
+          console.log('details', details_url);
+          new Crawler({
+            maxConnections: 10,
+            userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+            callback: function(error, result, $) {
+              if($){
+                var img = $('div.post-image').find('img').attr('src');
+                if(img === undefined || img === 'undefined'){
+                  
+                } else {
+                  img = img.substring(2, img.length);
+                  imgs.push(img);
+                }
+              }
+            }
+          }).queue(details_url);
+        })
+      }
+    }
+  });
+
+  c.queue('http://imgur.com/r/nsfw');
