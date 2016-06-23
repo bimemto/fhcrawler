81ca77346f828e8518b7fd4b8318a33100f53200
Nguyễn Khánh Duy <duynk@appota.com>
2016-06-23 10:25:46 +0700

    remove img craw

--- sondt_bot.js
+++ sondt_bot.js
@@ -270,32 +270,32 @@ function degToCompass(num) {
   return arr[(val % 16)];
 }
 
-var c3 = new Crawler({
-  maxConnections: 10,
-  userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
-  callback: function(error, result, $) {
-    if($){
-      $('div.posts.sub-gallery.br5.first-child').find('div.post').each(function(index, div){
-        var id = $(div).attr('id');
-        var details_url = 'http://imgur.com/r/nsfw/' + id;
-        new Crawler({
-          maxConnections: 10,
-          userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
-          callback: function(error, result, $) {
-            if($){
-              var img = $('div.post-image').find('img').attr('src');
-              if(img === undefined || img === 'undefined' || img === ''){
+// var c3 = new Crawler({
+//   maxConnections: 10,
+//   userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+//   callback: function(error, result, $) {
+//     if($){
+//       $('div.posts.sub-gallery.br5.first-child').find('div.post').each(function(index, div){
+//         var id = $(div).attr('id');
+//         var details_url = 'http://imgur.com/r/nsfw/' + id;
+//         new Crawler({
+//           maxConnections: 10,
+//           userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+//           callback: function(error, result, $) {
+//             if($){
+//               var img = $('div.post-image').find('img').attr('src');
+//               if(img === undefined || img === 'undefined' || img === ''){
 
-              } else {
-                img = img.substring(2, img.length);
-                imgs.push(img);
-              }
-            }
-          }
-        }).queue(details_url);
-      })
-    }
-  }
-});
+//               } else {
+//                 img = img.substring(2, img.length);
+//                 imgs.push(img);
+//               }
+//             }
+//           }
+//         }).queue(details_url);
+//       })
+//     }
+//   }
+// });
 
-c3.queue('http://imgur.com/r/nsfw');
+// c3.queue('http://imgur.com/r/nsfw');
