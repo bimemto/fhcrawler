680f9c1df47088fee838ff089de6a35ab362fefe
Nguyễn Khánh Duy <duynk@appota.com>
2016-07-15 14:16:26 +0700

    add tnl

--- bot_page.js
+++ bot_page.js
@@ -178,7 +178,17 @@ var stopListening = api.listen(function(err, event) {
             if (err) console.log(err);
           });
           callBotApi('tnl', function(result){
-            api.sendMessage(result, event.threadID);  
+            var length = result.length;
+            var str1 = result.substring(0, length/5);
+            var str2 = result.substring(length/5 + 1, length/(2/5));
+            var str3 = result.substring(length/(2/5) + 1, length/(3/5));
+            var str4 = result.substring(length/(3/5) + 1, length/(4/5));
+            var str5 = result.substring(length/(4/5) + 1, length);
+            api.sendMessage(str1, event.threadID);
+            api.sendMessage(str2, event.threadID);
+            api.sendMessage(str3, event.threadID);
+            api.sendMessage(str4, event.threadID);
+            api.sendMessage(str5, event.threadID);
           });
         }
       }
