4ef03db99abb8f7df819d7480c1ece891fdfa9f6
Nguyễn Khánh Duy <duynk@appota.com>
2016-07-21 11:52:53 +0700

    poke

--- pokecrawler.js
+++ pokecrawler.js
@@ -1,5 +1,4 @@
 var Crawler = require("crawler");
-var db = require("./db.js");
 
 var c = new Crawler({
 	maxConnections: 10,
@@ -19,8 +18,8 @@ var c = new Crawler({
     				type = $(type1).text() + '-' + $(type2).text();	
     			}
     			var image = 'https://img.pokemondb.net/artwork/' + name.toLowerCase() + '.jpg';
-    			console.log(id + ", " + name + ", " + image + ", " + type);
-   				//db.insertPokemon(id, name, image, type);
+    			//console.log(id + ", " + name + ", " + image + ", " + type);
+   				db.insertPokemon(id, name, image, type);
    			})
     	} else {
     		console.log('lol');
@@ -28,12 +27,29 @@ var c = new Crawler({
     }
 });
 
-c.queue('http://pokemondb.net/pokedex/national');
+//c.queue('http://pokemondb.net/pokedex/national');
 
-// db.connectDB(function(error, result) {
-// 	if (error) {
-// 		console.log(error);
-// 	} else {
-// 		c.queue('http://pokemondb.net/pokedex/national');
-// 	}
-// });
+connectDB(function(result) {
+	c.queue('http://pokemondb.net/pokedex/national');
+});
+
+var db_config = {
+    host: '103.53.170.173',
+    user: 'duynk',
+    password: 'smilelife',
+    database: 'Pokedex'
+};
+
+var connection;
+
+connectDB = function(callback) {
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+    connection.connect(function(error, result){
+        if(error){
+            console.log('error when connecting to db:', error);
+        } else {
+            console.log('connected to db', result);
+            callback(result);  
+        }
+    });
+};
