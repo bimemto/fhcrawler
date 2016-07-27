0d2259a8f5c4383ee908268aba200786aa1b04f3
Nguyễn Khánh Duy <duynk@appota.com>
2016-07-27 11:48:35 +0700

    f

--- poke_stats_crawler.js
+++ poke_stats_crawler.js
@@ -14,50 +14,51 @@ db.getPokemonList(function(rows){
     for (var i = 0; i < rows.length; i++) {
         var id = rows[i].id;
         var detailsUrl = 'http://pokemongo.gamepress.gg/pokemon/' + id;
-        c.queue([{
-            uri: detailsUrl,
-            callback: function (error, result, $) {
-                if($){
-                    $('div.pokemon-stats').find('tr').each(function(index, tr){
-                        switch(index){
-                            case 1:
-                                baseAtk = $(tr).find('td:not([class!=""])').text().trim();
-                                break;
-                            case 2:
-                                baseDef = $(tr).find('td:not([class!=""])').text().trim();
-                                break;
-                            case 3:
-                                baseSta = $(tr).find('td:not([class!=""])').text().trim();
-                                break;
-                            case 4:
-                                captureRate = $(tr).find('td:not([class!=""])').text().trim();
-                                break;
-                            case 5:
-                                fleeRate = $(tr).find('td:not([class!=""])').text().trim();
-                                break;
-                            case 6:
-                                type = $(tr).find('td:not([class!=""])').text().trim();
-                                break;
-                        }
-                    })
+        console.log(detailsUrl);
+        // c.queue([{
+        //     uri: detailsUrl,
+        //     callback: function (error, result, $) {
+        //         if($){
+        //             $('div.pokemon-stats').find('tr').each(function(index, tr){
+        //                 switch(index){
+        //                     case 1:
+        //                         baseAtk = $(tr).find('td:not([class!=""])').text().trim();
+        //                         break;
+        //                     case 2:
+        //                         baseDef = $(tr).find('td:not([class!=""])').text().trim();
+        //                         break;
+        //                     case 3:
+        //                         baseSta = $(tr).find('td:not([class!=""])').text().trim();
+        //                         break;
+        //                     case 4:
+        //                         captureRate = $(tr).find('td:not([class!=""])').text().trim();
+        //                         break;
+        //                     case 5:
+        //                         fleeRate = $(tr).find('td:not([class!=""])').text().trim();
+        //                         break;
+        //                     case 6:
+        //                         type = $(tr).find('td:not([class!=""])').text().trim();
+        //                         break;
+        //                 }
+        //             })
 
-                    $('div[id=evolution-requirements]').find('tr').each(function(index, tr){
-                        if(index === 0){
-                            candy = $(tr).find('td:not([class!=""])').text().trim();
-                        } else {
-                            hatchDistance = $(tr).find('td:not([class!=""])').text().trim();
-                        }
-                    })
+        //             $('div[id=evolution-requirements]').find('tr').each(function(index, tr){
+        //                 if(index === 0){
+        //                     candy = $(tr).find('td:not([class!=""])').text().trim();
+        //                 } else {
+        //                     hatchDistance = $(tr).find('td:not([class!=""])').text().trim();
+        //                 }
+        //             })
 
-                    var image = 'http://pokemongo.gamepress.gg/' + $('div.pokemon-image').find('img').attr('src');
-                    var pokeId = image.substring(image.lastIndexOf('/') + 1, image.lastIndexOf('.'));
-                    if(pokeId.indexOf('_') > -1){
-                        pokeId = pokeId.substring(0, pokeId.lastIndexOf('_'));
-                    }
-                    db.insertPokeStats(pokeId, image, baseAtk, baseDef, baseSta, captureRate, fleeRate, type, candy, hatchDistance);
-                }
-            }
-        }]);
+        //             var image = 'http://pokemongo.gamepress.gg/' + $('div.pokemon-image').find('img').attr('src');
+        //             var pokeId = image.substring(image.lastIndexOf('/') + 1, image.lastIndexOf('.'));
+        //             if(pokeId.indexOf('_') > -1){
+        //                 pokeId = pokeId.substring(0, pokeId.lastIndexOf('_'));
+        //             }
+        //             db.insertPokeStats(pokeId, image, baseAtk, baseDef, baseSta, captureRate, fleeRate, type, candy, hatchDistance);
+        //         }
+        //     }
+        // }]);
     }
 });
 
