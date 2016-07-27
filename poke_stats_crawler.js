eff37f8704aed4efbf67eee9c63909729b2f73e9
Nguyễn Khánh Duy <duynk@appota.com>
2016-07-27 10:48:23 +0700

    add poke cp

--- poke_stats_crawler.js
+++ poke_stats_crawler.js
@@ -10,46 +10,45 @@ db.getPokemonList(function(rows){
     for (var i = 0; i < rows.length; i++) {
         var id = rows[i].id;
         var detailsUrl = 'http://pokemongo.gamepress.gg/pokemon/' + id;
-        console.log(detailsUrl);
-        // new Crawler({
-        //     maxConnections: 10,
-        //     callback: function(error, result, $) {
-        //         if($){
-        //             $('div.pokemon-stats').find('tr').each(function(index, tr){
-        //                 switch(index){
-        //                     case 1:
-        //                         baseAtk = $(tr).find('td:not([class!=""])').text();
-        //                         break;
-        //                     case 2:
-        //                         baseDef = $(tr).find('td:not([class!=""])').text();
-        //                         break;
-        //                     case 3:
-        //                         baseSta = $(tr).find('td:not([class!=""])').text();
-        //                         break;
-        //                     case 4:
-        //                         captureRate = $(tr).find('td:not([class!=""])').text();
-        //                         break;
-        //                     case 5:
-        //                         fleeRate = $(tr).find('td:not([class!=""])').text();
-        //                         break;
-        //                     case 6:
-        //                         type = $(tr).find('td:not([class!=""])').text();
-        //                         break;
-        //                 }
-        //             })
+        new Crawler({
+            maxConnections: 10,
+            callback: function(error, result, $) {
+                if($){
+                    $('div.pokemon-stats').find('tr').each(function(index, tr){
+                        switch(index){
+                            case 1:
+                                baseAtk = $(tr).find('td:not([class!=""])').text().trim();
+                                break;
+                            case 2:
+                                baseDef = $(tr).find('td:not([class!=""])').text().trim();
+                                break;
+                            case 3:
+                                baseSta = $(tr).find('td:not([class!=""])').text().trim();
+                                break;
+                            case 4:
+                                captureRate = $(tr).find('td:not([class!=""])').text().trim();
+                                break;
+                            case 5:
+                                fleeRate = $(tr).find('td:not([class!=""])').text().trim();
+                                break;
+                            case 6:
+                                type = $(tr).find('td:not([class!=""])').text().trim();
+                                break;
+                        }
+                    })
 
-        //             $('div.evolution-requirements').find('tr').each(function(index, tr){
-        //                 if(index === 0){
-        //                     candy = $(tr).find('td:not([class!=""])').text().trim();
-        //                 } else {
-        //                     hatchDistance = $(tr).find('td:not([class!=""])').text().trim();
-        //                 }
-        //             })
-        //             console.log(id + ', ' + baseAtk + ', ' + baseDef + ', ' + baseSta + ', ' + captureRate + ', ' + fleeRate + ', ' + type + ', '+ candy + ', ' + hatchDistance);
-        //             //db.insertPokeStats(id, baseAtk, baseDef, baseSta, captureRate, fleeRate, type, candy, hatchDistance);
-        //         }
-        //     }
-        // }).queue(detailsUrl);
+                    $('div.evolution-requirements').find('tr').each(function(index, tr){
+                        if(index === 0){
+                            candy = $(tr).find('td:not([class!=""])').text().trim();
+                        } else {
+                            hatchDistance = $(tr).find('td:not([class!=""])').text().trim();
+                        }
+                    })
+                    console.log(id + ', ' + baseAtk + ', ' + baseDef + ', ' + baseSta + ', ' + captureRate + ', ' + fleeRate + ', ' + type + ', '+ candy + ', ' + hatchDistance);
+                    //db.insertPokeStats(id, baseAtk, baseDef, baseSta, captureRate, fleeRate, type, candy, hatchDistance);
+                }
+            }
+        }).queue(detailsUrl);
     }
 });
 
