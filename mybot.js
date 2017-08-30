b73c7459bd77334fd9e0ab54f4a5a8fd33db4131
duynk <duynk@appota.com>
2017-08-30 17:09:34 +0700

    add my bot

--- mybot.js
+++ mybot.js
@@ -172,9 +172,11 @@ fs.exists('mybot.json', (exists) => {
             });
             if(event.senderID === '100000404491080' || event.senderID === '100005017784835' || event.senderID === '100002542018182'){
               if(event.body.length > 0){
-                api.sendMessage({
-                  body: event.body
-                }, event.threadID);
+                if(event.body.indexOf('duy') < 0 || event.body.indexOf('Duy') < 0 || event.body.indexOf('lợi') < 0 || event.body.indexOf('Lợi') < 0){
+                  api.sendMessage({
+                    body: event.body
+                  }, event.threadID);
+                }
               } else {
                 if(event.attachments){
                   if(event.attachments[0].type === 'sticker'){
