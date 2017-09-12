64cb0c4935888527f8c352e780cf4dd8ac531710
duynk <duynk@appota.com>
2017-09-12 11:52:04 +0700

    https

--- bot_center.js
+++ bot_center.js
@@ -286,7 +286,7 @@ app.get('/bot/center',function(req, res){
 			'Accepted-Language': 'en_US'
     },
 		json: requestData,
-    uri: 'https://sandbox-api.uber.com/v1.2/requests/estimate',
+    uri: 'https://api.uber.com/v1.2/requests/estimate',
     method: 'POST'
   }, function (err, response, body) {
     //it works!
@@ -313,7 +313,7 @@ app.get('/bot/center',function(req, res){
       'Content-Type': 'application/json'
     },
 		json: requestData,
-    uri: 'https://sandbox-api.uber.com/v1.2/requests',
+    uri: 'https://api.uber.com/v1.2/requests',
     method: 'POST'
   }, function (err, response, body) {
     //it works!
@@ -329,7 +329,7 @@ app.get('/bot/center',function(req, res){
 		headers: {
       'Authorization': 'Bearer KA.eyJ2ZXJzaW9uIjoyLCJpZCI6IlprMmFLK3FMUnltV0dlOHJPbnhGTlE9PSIsImV4cGlyZXNfYXQiOjE1MDc3MTc1MzUsInBpcGVsaW5lX2tleV9pZCI6Ik1RPT0iLCJwaXBlbGluZV9pZCI6MX0.rgA8fdbOQ5IL5QU1G_G5WUXQ6FkxGe_LQHDCh__E6Dg',
     },
-		uri: 'https://sandbox-api.uber.com/v1.2/requests/current',
+		uri: 'https://api.uber.com/v1.2/requests/current',
 		method: 'GET'
 	}, function(err, response, body){
 		if(err){
