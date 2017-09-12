9569288f59062b220f5e805a53dbb0ab11e13bb0
duynk <duynk@appota.com>
2017-09-12 11:32:22 +0700

    https

--- bot_center.js
+++ bot_center.js
@@ -343,7 +343,7 @@ app.get('/bot/center',function(req, res){
 });
 
 app.use(express.static('static'));
-app.listen(80);
+app.listen(8080);
 
 // var server = app.listen(6868, function() {
 // 	var host = server.address().address
