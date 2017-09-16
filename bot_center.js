5b9ab6a9d7b8bd4919dbf4ed6e39e9e62130b5cb
duynk <duynk@appota.com>
2017-09-16 10:32:19 +0700

    https

--- bot_center.js
+++ bot_center.js
@@ -346,8 +346,6 @@ app.get('/bot/center',function(req, res){
 }
 });
 
-//app.use(express.static('static'));
-//app.listen(6868);
 const https = require('https');
 // Set up express server here
 app.use(require('helmet')());
@@ -358,21 +356,10 @@ const options = {
 app.listen(6868);
 https.createServer(options, app).listen(8443);
 
-// var server = app.listen(6868, function() {
-// 	var host = server.address().address
-// 	var port = server.address().port
-// 	console.log("Example app listening at http://%s:%s", host, port);
-// })
-
-// greenlock.create({
-//   server: 'staging',
-//   email: 'duydkny@gmail.com',
-//   agreeTos: true,
-//   approveDomains: ['bu.1ly.co'],
-//   app: app.use('/', function (req, res) {
-//     res.end('Hello, World!');
-//   })
-// }).listen(8080, 8443);
+app.post('/uber/hook', function(request, response){
+	console.log('request: ', request);
+	console.log('response: ', response);
+})
 
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
